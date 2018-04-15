821e8afbeb9f3d513d5d13341b57b5ad5fa2ea71
dwisniewski1993 <dwisniewski1993@tlen.pl>
2018-04-15 23:49:57 +0200

    Genetic Algorithm initial commit

--- src/main/java/GA/App.java
+++ src/main/java/GA/App.java
@@ -1,5 +1,13 @@
 package GA;
 
+import org.jenetics.*;
+import org.jenetics.engine.Engine;
+import org.jenetics.engine.EvolutionStatistics;
+import static org.jenetics.engine.EvolutionResult.toBestPhenotype;
+import org.jenetics.util.Factory;
+import org.jenetics.util.ISeq;
+
+import java.util.Random;
 import java.util.Set;
 
 /**
@@ -8,6 +16,16 @@ import java.util.Set;
  */
 public class App 
 {
+    //Fitness Function
+    private static int FF(final Genotype<EnumGene<Integer>> gt){
+        Random rand = new Random();
+        int maximum = 15;
+        int minimum = 1;
+        int n = maximum - minimum +1;
+        int i = rand.nextInt() % n;
+        int randomnum = minimum +i;
+        return randomnum;
+    }
     public static void main( String[] args )
     {
         System.out.println( "Genetic Algorithm!" );
@@ -24,5 +42,30 @@ public class App
         Set<Integer> idList = ontology.getIDArrays();
 
         for (int x: idList){System.out.println(x);}
+
+        //GA Init
+        ISeq<Integer> allele = ISeq.of(idList);
+        Factory<Genotype<EnumGene<Integer>>> gtf = Genotype.of(PermutationChromosome.of(allele));
+
+        //Genetic Algorithm
+        final Engine<EnumGene<Integer>, Integer> engine = Engine.builder(App::FF, gtf)
+                .populationSize(5) //Population Size
+                .maximalPhenotypeAge(10) // Phenotyope Age
+                .survivorsSelector(new RouletteWheelSelector<>()) //Survive Selector
+                .offspringSelector(new TournamentSelector<>()) // Offspring selector
+                .alterers(new PartiallyMatchedCrossover<>(0.6), new SwapMutator<>(0.2)) //Crossover & mutation rate
+                .build();
+
+        //Evolution statistics consumer
+        final EvolutionStatistics<Integer, ?> stats = EvolutionStatistics.ofNumber();
+
+        final Phenotype<EnumGene<Integer>, Integer> best = engine
+                .stream()
+                .limit(20)
+                .peek(stats)
+                .collect(toBestPhenotype());
+
+        System.out.println(stats); // GA stats
+        System.out.println(best); // Best Phenotype
     }
 }
