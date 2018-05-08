d6d39b3a0d9a483404573273f4022dbdf5b327ce
s396455 <domwis@st.amu.edu.pl>
2018-05-08 10:41:18 +0200

    Add correct prediction

--- src/main/java/NeuroGen/App.java
+++ src/main/java/NeuroGen/App.java
@@ -1,13 +1,12 @@
 package NeuroGen;
 
-import NeuroGen.GA.GeneticAlgorithm;
 import NeuroGen.GA.SemanticHandler;
-import org.datavec.api.records.Record;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.util.ndarray.RecordConverter;
 import org.datavec.api.writable.Writable;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.datavec.RecordReaderMultiDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -18,23 +17,19 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.jenetics.*;
-import org.jenetics.engine.Engine;
-import org.jenetics.engine.EvolutionStatistics;
-import static org.jenetics.engine.EvolutionResult.toBestPhenotype;
-import org.jenetics.util.Factory;
-import org.jenetics.util.ISeq;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 
 /**
@@ -63,7 +58,7 @@ public class App
         int seed = 123;
         double learning_rate = 0.01;
         int batchSize = 50;
-        int nEpochs = 30;
+        int nEpochs = 100;
         int numInputs = 2;
         int numOutputs = 2;
         int numHiddenNodes = 20;
@@ -88,7 +83,7 @@ public class App
                 .iterations(1)
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .learningRate(learning_rate)
-                .updater(Updater.NESTEROVS)
+                .updater(Updater.ADAM)
                 .momentum(0.9)
                 .list()
                 .layer(0, new DenseLayer.Builder()
@@ -99,7 +94,7 @@ public class App
                     .build())
                 .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
                     .weightInit(WeightInit.XAVIER)
-                        .activation(Activation.IDENTITY)
+                        .activation(Activation.SIGMOID)
                         .weightInit(WeightInit.XAVIER)
                         .nIn(numHiddenNodes)
                         .nOut(10)
@@ -115,14 +110,16 @@ public class App
             model.fit(iterator);
         }
 
+
         RecordReader sampleReader = new CSVRecordReader(numLinesToSkip, fileDelimeter);
-        sampleReader.initialize(new FileSplit(new File("samples.csv")));
+        sampleReader.initialize(new FileSplit(new File("sample.csv")));
+
         List<Writable> record = sampleReader.next();
+        System.out.println("Record list: "+record);
         INDArray convert = RecordConverter.toArray(record);
-        int[] predicted = model.predict(convert);
-
-        System.out.println(predicted[0]);
-
+        System.out.println("Convert Array: "+convert);
+        INDArray output = model.output(convert);
+        System.out.println("Predicted INDArray: "+output);
 
         System.out.println("Make NeuroGen:GA");
         //GeneticAlgorithm genAlg = new GeneticAlgorithm(50, 15, 0.7, 0.2, 100, idList, semanticFilePAth);
